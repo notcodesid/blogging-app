c08a36a7ac3d9aae78872b236cc263553afede58
Sidharth pandro <sidharthpandro50@gmail.com>
2024-06-23 16:21:50 +0530

    sign up route & jwt auth

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,43 @@
 import { Hono } from 'hono'
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
+import { sign } from 'hono/jwt'
 
-const app = new Hono()
+const app = new Hono<{
+  Bindings : {
+    DATABASE_URL : string
+	JWT_SECRET : string,
+  }
+}>()
+
+app.get("/" , (c) => {
+	return c.text("Hello Hono")
+})
+
+app.post('/api/v1/signup', async (c) => {
+	const prisma = new PrismaClient({
+		datasourceUrl: c.env?.DATABASE_URL	,
+	}).$extends(withAccelerate());
+
+	const body = await c.req.json();
+    console.log('Received body:', body);
+	try {
+		const user = await prisma.user.create({
+			data: {
+				email: body.email,
+				password: body.password
+			}
+		});
+		const jwt = await sign({ id: user.id }, c.env.JWT_SECRET);
+		return c.json({ jwt });
+	} catch(e) {
+    console.log(e)
+	console.error('Error during signup:', e);
+		c.status(403);
+		return c.json({ error: "error while signing up" });
+	}
+})
+
 
 app.get('/api/v1/blog/:id', (c) => {
   return c.text('Get Blog')
@@ -10,7 +47,7 @@ app.get('/api/v1/blog/bulk' , (c) => {
   return c.text('All the Blog')
 })
 
-app.post('/api/v1/user/signup' , (c) => {
+app.post('/api/v1/user/signin' , (c) => {
   return c.text('Sign up')
 })
 
